7783c48e9756ef516bf040f39de115a744d8ee1e
Gautham Venkataraman <gautham@dexterenergy.ai>
2025-11-01 09:01:26 +0100

    chore: local setup

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              openssl
+              pkg-config
+              rustc
+              cargo
+            ]
+            ++ lib.optional stdenv.isDarwin libiconv;
+          };
+      }
+    );
+}
